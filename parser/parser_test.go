7f8044f252fc35555ce3140f392b9203edae9f7b
Konrad <Konkord720@gmail.com>
2024-06-19 23:46:51 +0200

    Added parsing of the let statements to the parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -10,20 +10,21 @@ import (
 
 func TestLetStatements(t *testing.T) {
     input := `
-let x = 5;
-let y = 10;
-let foobar = 838383;
-`
+    let x = 5;
+    let y = 10;
+    let foobar = 838383;
+    `
     l := lexer.New(input)
     p := New(l)
 
     program := p.ParseProgram()
+    checkParserErrors(t, p)
     if program == nil {
         t.Fatalf("ParseProgram() returned nil")
     }
     if len(program.Statements) != 3 {
         t.Fatalf("program.Statements does not contain 3 statements. got=%d",
-            len(program.Statements))
+        len(program.Statements))
     }
 
     tests := []struct {
@@ -40,6 +41,19 @@ let foobar = 838383;
             return
         }
     }
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+    errors := p.Errors()
+    if len(errors) == 0 { 
+        return
+    }   
+
+    t.Errorf("Parser has %d errors", len(errors))
+    for _, msg := range errors {
+        t.Errorf("parser error: %q", msg)
+    }
+    t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
@@ -61,7 +75,7 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 
     if letStmt.Name.TokenLiteral() != name {
         t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s",
-            name, letStmt.Name.TokenLiteral())
+        name, letStmt.Name.TokenLiteral())
         return false
     }
 
